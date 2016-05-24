687da7d90e837c9624b54de859afc74206cc6969
Matthieu <matthieuy@riseup.net>
2016-05-24 19:17:35 +0200

    Use tar instead of unzip

--- install.sh
+++ install.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 #
 # Script permettant l'installation de pack de scripts
+# Run :  wget https://raw.githubusercontent.com/geekariom/recal-script/master/install.sh -O - | bash
 #
 
 destdir=/recalbox/share/tools/geekariom
-tmpfile=~/script.zip
+tmpfile=~/script.tar.gz
 
 
 echo " => Téléchargement des scripts"
-wget -O ${tmpfile} --show-progress --quiet https://github.com/geekariom/recal-script/archive/master.zip
+wget -O ${tmpfile} --show-progress --quiet https://github.com/geekariom/recal-script/archive/master.tar.gz
+
+echo " => Création des dossiers"
+if [ -e ${destdir} ]; then
+    rm -Rf ${destdir}
+fi
 mkdir -p ${destdir}
 
 echo " => Décompression"
-unzip -o -d "${destdir}" -q ${tmpfile}
+tar -xzf ${tmpfile} --strip=1 -C ${destdir}
 
 echo " => Suppression des fichiers temporaires"
-#rm ${tmpfile}
+rm ${tmpfile}
 
 echo " => Fin de l'installation"
 echo ""
-read -p "Voulez-vous lancer le script maintenant ? (o/n)" ask
+read -p "Voulez-vous lancer le script maintenant ? (o/n) " ask
 if [ $ask = 'o' ]; then
     bash ${destdir}/main.sh
 fi
